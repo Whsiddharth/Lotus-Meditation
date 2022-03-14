804b8b44864b39b59d32ab7add86aa24da7d55e8
3 <79239887+Whsiddharth@users.noreply.github.com>
2022-03-14 13:06:21 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function breath(){
+    window.location="breath.html"
+}
+function stretch(){
+    window.location="stretch.html"
+}
+function tips(){
+    window.location="tips.html"
+}
+function home(){
+    window.location="index.html"
+}
